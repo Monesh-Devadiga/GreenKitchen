b9fbb2fd15b1981553275b1d9edfd42a62f7b7e0
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-02-07 23:39:55 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE IF NOT EXISTS recipes_db3;
 USE recipes_db3;
 
+-- This is the tabale defenition to store the all registered users
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
