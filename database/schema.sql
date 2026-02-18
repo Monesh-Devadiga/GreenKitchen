3b74610ce57b694a61a46ff1bbe587ad6917b02d
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-02-18 18:22:03 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -28,7 +28,8 @@ CREATE TABLE cuisines
 );
 
 -- This is the tabale defenition to store the different recipes.
-CREATE TABLE recipes (
+CREATE TABLE recipes 
+(
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
