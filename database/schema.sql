f494505b046c246c910f6edf4c9c7cdd6dd041ce
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-02-19 16:35:16 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -77,7 +77,7 @@ CREATE TABLE instructions
 
 -- This is the tabale defenition to store the different tags.
 CREATE TABLE tags 
-  (
+(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) UNIQUE NOT NULL
 );
