bd6b24019ed31842dc38f675c050088469fd4b14
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-02-20 22:18:30 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,7 @@
 -- The entire scheme is for overall existed tables.
 
 CREATE DATABASE IF NOT EXISTS recipes_db3;
-USE recipes_db3;    -- The database which is used to store the data
+USE recipes_db3;    -- The database which is used to store the data.
 
 -- This is the tabale defenition to store the all registered users.
 CREATE TABLE users
