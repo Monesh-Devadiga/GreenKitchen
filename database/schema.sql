4305861fde6a9bbd97871ba53ea91f3ecd1e0824
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-02-20 22:20:51 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,8 @@
 
 -- To create the database.
 CREATE DATABASE IF NOT EXISTS recipes_db3;
+
+-- The database which is used to store the data.
 USE recipes_db3;
 
 -- The entire scheme is for overall existed tables.
