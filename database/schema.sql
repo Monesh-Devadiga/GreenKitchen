1d3d8fad4342f14fe7ed249fd6c8f189bbb0c56c
Monesh Devadiga <moneshdevadiga28@gmail.com>
2026-04-08 23:51:15 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,6 @@
                          -- This defines the overall defination of datbase scheme which stores data in MySQL Database Software.
   
--- To create the database.
+-- To create the database. 
 CREATE DATABASE IF NOT EXISTS recipes_db3;
  
 -- The database which is used to store the data.
